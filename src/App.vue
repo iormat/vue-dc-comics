d85f48868fdbc7f866aa074d6ad9894ae5e32381
iormat <iorio436@outlook.it>
2021-11-24 15:49:11 +0100

    header component update

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <Header/>
+
+        <Footer/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue';
+
+import Footer from './components/Header.vue';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Footer
   }
 }
 </script>
 
 <style lang="scss">
+* {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    font-size: 20px;
+}
+.container-center {
+    width: 80%;
+    margin: 0 auto;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,116 @@
+<template>
+    <header>
+        <div class="container-center">
+            <section>
+                <img src="../assets/img/dc-logo.png" alt="">
+            </section>
+            <section class="header-nav">
+                <nav>
+                    <ul>
+                        <li v-for="(link, i) in links" :key="i"> 
+                            <a :class="link.selected ? 'active' : '' " href="link.url">{{ link.text }}</a> 
+                        </li>
+                    </ul>
+                </nav>
+            </section>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    data(){
+        return {
+            links : [
+                {
+                    url : "#",
+                    text : "characters",
+                    selected : false,
+                },
+                {
+                    url : "#",
+                    text : "comics",
+                    selected : true,
+                },
+                {
+                    url : "#",
+                    text : "movies",
+                    selected : false,
+                },
+                {
+                    url : "#",
+                    text : "tv",
+                    selected : false,
+                },
+                {
+                    url : "#",
+                    text : "games",
+                    selected : false,
+                },
+                {
+                    url : "#",
+                    text : "collectibles",
+                    selected : false,
+                },
+                {
+                    url : "#",
+                    text : "videos",
+                    selected : false,
+                },
+                {
+                    url : "#",
+                    text : "fans",
+                    selected : false,
+                },
+                {
+                    url : "#",
+                    text : "news",
+                    selected : false,
+                },
+                {
+                    url : "#",
+                    text : "shop",
+                    selected : false,
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+    header {
+        padding: 20px 0;
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+    }
+    .container-center {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        & img {
+            width: 70px;
+        }
+    }
+    li {
+        display: inline-block;
+        margin: 0 20px;
+        & a {
+            padding: 20px 0;
+            color: #000;
+            text-decoration: none;
+            text-transform: uppercase;
+            font-size: .8rem;
+            font-weight: bold;
+        }
+        & a.active, a:hover {
+            color: #377AE4;
+            border-bottom: 3px solid #377AE4;
+        }
+
+    }
+</style>
